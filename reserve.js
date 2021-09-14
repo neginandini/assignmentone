ed91e464fe9ad9078999db2ec14549c99ff503f1
neginandini <nandini.negi@neosoftmail.com>
2021-09-14 17:58:00 +0530

    first

---
+++ reserve.js
@@ -0,0 +1,8 @@
+$(function(){$('#resbtn').mouseover(function(){
+    $(this).css({"transform":"scale(1.05,1.05)","transition":"0.8s"});
+});
+$('#resbtn').mouseout(function(){
+    $(this).css({"transform":"none","transition":"0.7s"});
+});
+
+});
